f4d1ff55d5205061ad8fefc2519630839bfc7ecc
Barak Bercovitz <barak@wizery.com>
2013-08-04 00:51:37 +0300

    major rewrite

--- build.sh
+++ build.sh
@@ -1,250 +1,432 @@
-function setup_environment() 
+export PATH__ROOT=`pwd`
+. configuration.sh
+
+function assert_no_error()
 {
-    export PROCESSORS_NUMBER=$(egrep '^processor' /proc/cpuinfo | wc -l)
-    export PATH__ROOT=`pwd`
-    . configuration.sh
+	if [ $? -ne 0 ]; then
+		echo "****** ERROR *******"
+		exit 1
+	fi
+}
 
-    export CROSS_COMPILE=arm-none-linux-gnueabi-
-    export ARCH=arm
-    export TOOLCHAIN_PATH=${PATH__TOOLCHAIN}/arm/bin
-    export PATH=$TOOLCHAIN_PATH:$PATH
-    export PKG_CONFIG_PATH=${PATH__FILESYSTEM}/lib/pkgconfig
+function repo_id()
+{
+	i="0"
+	while [ $i -lt ${#repositories[@]} ]; do
+		[ $1 == "${repositories[i]}" ] && echo $i
+		i=$[$i + 3]
+	done
+}
 
-    export INSTALL_PREFIX=${PATH__FILESYSTEM}
-    export LIBNL_PATH=${PATH__SRC__LIBNL}
-    export KERNEL_PATH=${PATH__SRC__KERNEL}
-    export KLIB=${KERNEL_PATH}
-    export KLIB_BUILD=${KERNEL_PATH}
-    export GIT_TREE=${PATH__SRC__DRIVER}
-    export GIT_COMPAT_TREE=${PATH__SRC__COMPAT}
+function repo_url()
+{
+	echo "${repositories[`repo_id $1` + 1]}"
+}
+
+function repo_branch()
+{
+	echo "${repositories[`repo_id $1` + 2]}"
+}
+
+function path()
+{
+	i="0"
+	while [ $i -lt "${#paths[@]}" ]; do
+		[ $1 == "${paths[i]}" ] && echo "${paths[i + 1]}"
+		i=$[$i + 2]
+	done
+}
+
+function repo_path()
+{
+	echo `path src`/$1
+}
+
+function cd_path()
+{
+	cd `path $1`
+}
+
+function cd_repo()
+{
+	cd `repo_path $1`
+}
+
+function cd_back()
+{
+	cd - > /dev/null
+}
+
+#----------------------------------------------------------j
+function setup_environment()
+{
+	export PROCESSORS_NUMBER=$(egrep '^processor' /proc/cpuinfo | wc -l)
+	export CROSS_COMPILE=arm-none-linux-gnueabi-
+	export ARCH=arm
+	export TOOLCHAIN_PATH=`path toolchain`/arm/bin
+	export PKG_CONFIG_PATH=`path filesystem`/lib/pkgconfig
+	export INSTALL_PREFIX=`path filesystem`
+	export LIBNL_PATH=`repo_path libnl`
+	export KERNEL_PATH=`repo_path kernel`
+	export KLIB=${KERNEL_PATH}
+	export KLIB_BUILD=${KERNEL_PATH}
+	export GIT_TREE=`repo_path driver`
+	export GIT_COMPAT_TREE=`repo_path compat`
+
+	export PATH=$TOOLCHAIN_PATH:$PATH
 }
 
 function setup_filesystem_skeleton()
 {
-	mkdir -p ${PATH__FILESYSTEM}/home/root
-	mkdir -p ${PATH__FILESYSTEM}/etc
+	mkdir -p `path filesystem`/home/root
+	mkdir -p `path filesystem`/etc
+	mkdir -p `path filesystem`/usr/lib/crda
+	mkdir -p `path filesystem`/lib/firmware/ti-connectivity
+	mkdir -p `path filesystem`/usr/share/wl18xx
 }
 
 function setup_directories()
 {
+	i="0"
+	while [ $i -lt ${#paths[@]} ]; do
+		mkdir -p ${paths[i + 1]}
+		i=$[$i + 2]
+	done
 	setup_filesystem_skeleton
-	mkdir -p ${PATH__OUTPUTS}
-	mkdir -p ${PATH__TOOLCHAIN}
-    mkdir -p ${PATH__TFTP}
-    mkdir -p ${PATH__DOWNLOADS}
-    mkdir -p ${PATH__SRC}
-}
 
-function setup_toolchain()
-{
-	[ ! -f ${PATH__DOWNLOADS}/arm-toolchain.tar.bz2 ] && wget https://sourcery.mentor.com/GNUToolchain/package6488/public/arm-none-linux-gnueabi/arm-2010q1-202-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2 -O ${PATH__DOWNLOADS}/arm-toolchain.tar.bz2
-    [ ! -d ${PATH__TOOLCHAIN}/arm ] && tar -xjf ${PATH__DOWNLOADS}/arm-toolchain.tar.bz2 -C ${PATH__TOOLCHAIN}    
-    [ ! -d ${PATH__TOOLCHAIN}/arm ] && mv ${PATH__TOOLCHAIN}/* ${PATH__TOOLCHAIN}/arm
-    
 }
 
 function setup_repositories()
 {
-	[ -z $NO_CLONE ] && git clone ${REPO__URL__CONFIGURATION} ${PATH__SRC__CONFIGURATION}
-	[ -z $NO_CLONE ] && git clone ${REPO__URL__KERNEL}  ${PATH__SRC__KERNEL}
-    [ -z $NO_CLONE ] && git clone ${REPO__URL__DRIVER}  ${PATH__SRC__DRIVER}
-    [ -z $NO_CLONE ] && git clone ${REPO__URL__HOSTAP}  ${PATH__SRC__HOSTAP}
-    [ -z $NO_CLONE ] && git clone ${REPO__URL__COMPAT}  ${PATH__SRC__COMPAT}
-    [ -z $NO_CLONE ] && git clone ${REPO__URL__COMPAT_WIRELESS} ${PATH__SRC__COMPAT_WIRELESS}
-    [ -z $NO_CLONE ] && git clone ${REPO__URL__CRDA}    ${PATH__SRC__CRDA}
-    [ -z $NO_CLONE ] && git clone ${REPO__URL__WIRELESS_REGDB}  ${PATH__SRC__WIRELESS_REGDB}
-    [ -z $NO_CLONE ] && git clone ${REPO__URL__OPENSSL} ${PATH__SRC__OPENSSL}
-    [ -z $NO_CLONE ] && git clone ${REPO__URL__LIBNL} ${PATH__SRC__LIBNL}
-	[ -z $NO_CLONE ] && git clone ${REPO__URL__TI_UTILS} ${PATH__SRC__TI_UTILS}
-    [ -z $NO_CLONE ] && git clone ${REPO__URL__FW_DOWNLOAD} ${PATH__SRC__FW_DOWNLOAD}   
-    [ -z $NO_CLONE ] && git clone ${REPO__URL__SCRIPTS_DOWNLOAD} ${PATH__SRC__SCRIPTS_DOWNLOAD}
+	i="0"
+	while [ $i -lt ${#repositories[@]} ]; do
+		url=${repositories[$i + 1]}
+		name=${repositories[$i]}
+		[ ! -d `repo_path $name` ] && git clone $url `repo_path $name`
+		i=$[$i + 3]
+	done
 }
 
 function setup_branches()
 {
-	cd ${PATH__SRC__CONFIGURATION};   git checkout ${REPO__BRANCH__CONFIGURATION};   cd -
-    cd ${PATH__SRC__KERNEL};          git checkout ${REPO__BRANCH__KERNEL};          cd -
-    cd ${PATH__SRC__DRIVER};          git checkout ${REPO__BRANCH__DRIVER};          cd -
-    cd ${PATH__SRC__HOSTAP};          git checkout ${REPO__BRANCH__HOSTAP};          cd -
-    cd ${PATH__SRC__COMPAT};          git checkout ${REPO__BRANCH__COMPAT};          cd -
-    cd ${PATH__SRC__COMPAT_WIRELESS}; git checkout ${REPO__BRANCH__COMPAT_WIRELESS}; cd -
-    cd ${PATH__SRC__OPENSSL};         git checkout ${REPO__BRANCH__OPENSSL};         cd -
-    cd ${PATH__SRC__LIBNL};           git checkout ${REPO__BRANCH__LIBNL};           cd -    
-    cd ${PATH__SRC__CRDA};            git checkout ${REPO__BRANCH__CRDA};            cd -
-    cd ${PATH__SRC__WIRELESS_REGDB};  git checkout ${REPO__BRANCH__WIRELESS_REGDB};  cd -
-	cd ${PATH__SRC__TI_UTILS};        git checkout ${REPO__BRANCH__TI_UTILS};        cd -
-    cd ${PATH__SRC__FW_DOWNLOAD};     git checkout ${REPO__BRANCH__FW_DOWNLOAD};     cd -
-    cd ${PATH__SRC__SCRIPTS_DOWNLOAD};git checkout ${REPO__BRANCH__SCRIPTS_DOWNLOAD};cd -
+	i="0"
+	while [ $i -lt ${#repositories[@]} ]; do
+		name=${repositories[$i]}
+		branch=${repositories[$i + 2]}
+		cd_repo $name
+		git checkout $branch
+		cd_back
+		i=$[$i + 3]
+	done
+}
+
+function setup_toolchain()
+{
+	if [ ! -f `path downloads`/arm-toolchain.tar.bz2 ]; then
+		wget ${toolchain[0]} -O `path downloads`/arm-toolchain.tar.bz2
+		tar -xjf `path downloads`/arm-toolchain.tar.bz2 -C `path toolchain`
+		mv `path toolchain`/* `path toolchain`/arm
+	fi
 }
 
 function configure_kernel()
 {
-    cd ${PATH__SRC__KERNEL}
-    [ -z $NO_CONFIG ] && rm .config
-    [ -z $NO_CONFIG ] && rm .config.old
-   [ -z $NO_CONFIG ] && cp ${PATH__SRC__CONFIGURATION}/kernel.config .config
-    [ -z $NO_CONFIG ] && yes "" 2>/dev/null | make oldconfig >/dev/null
-    cd -
+	cd_repo kernel
+	rm .config
+	rm .config.old
+	cp `repo_path configuration`/kernel.config .config
+	yes "" 2>/dev/null | make oldconfig >/dev/null
+	assert_no_error
+	cd_back
 }
 
 function build_uimage()
 {
-    cd ${PATH__SRC__KERNEL}
-    [ -z $NO_CLEAN ] && make clean
-    make -j${PROCESSORS_NUMBER} uImage
-    LOADADDR=0x80008000 make -j${PROCESSORS_NUMBER} uImage-dtb.am335x-evm
-    cp ${PATH__SRC__KERNEL}/arch/arm/boot/uImage-dtb.am335x-evm ${PATH__TFTP}/uImage
-    cd -
+	cd_repo kernel
+	[ -z $NO_CLEAN ] && make clean
+	[ -z $NO_CLEAN ] && assert_no_error
+	make -j${PROCESSORS_NUMBER} uImage
+	assert_no_error
+	LOADADDR=0x80008000 make -j${PROCESSORS_NUMBER} uImage-dtb.am335x-evm
+	assert_no_error
+	cp `repo_path kernel`/arch/arm/boot/uImage-dtb.am335x-evm `path tftp`/uImage
+	cd_back
 }
 
 function build_modules()
 {
-    cd ${PATH__SRC__COMPAT_WIRELESS}
-    git reset --hard HEAD
-    [ -z $NO_CLEAN ] && make clean
-    [ -z $NO_CLEAN ] && rm .compat* MAINTAINERS Makefile.bk .compat_autoconf_
-    [ -z $NO_CLEAN ] && ./scripts/admin-refresh.sh network
-    [ -z $NO_CLEAN ] && ./scripts/driver-select wl18xx
-    make -j${PROCESSORS_NUMBER}
-    find . -name \*.ko -exec ${STRIP} -g {} \;
-    make -C ${KERNEL_PATH} M=`pwd` "INSTALL_MOD_PATH=${PATH__FILESYSTEM}" modules_install
-    #chmod -R 0777 ${PATH__FILESYSTEM}/lib/modules/
-    cd -
+	cd_repo compat_wireless
+	if [ -z $NO_CLEAN ]; then
+		git reset --hard HEAD
+		make clean
+		#assert_no_error
+		rm .compat* MAINTAINERS Makefile.bk .compat_autoconf_
+	fi
+	[ -z $NO_CONFIG ] && ./scripts/admin-refresh.sh network
+	[ -z $NO_CONFIG ] && ./scripts/driver-select wl18xx
+	make -j${PROCESSORS_NUMBER}
+	assert_no_error
+	find . -name \*.ko -exec cp {} `path debugging`/ \;
+	find . -name \*.ko -exec ${CROSS_COMPILE}strip -g {} \;
+	make -C ${KERNEL_PATH} M=`pwd` "INSTALL_MOD_PATH=`path filesystem`" modules_install
+	assert_no_error
+	#chmod -R 0777 ${PATH__FILESYSTEM}/lib/modules/
+	cd_back
 }
 
 function build_openssl()
 {
-    cd ${PATH__SRC__OPENSSL}
-    [ -z $NO_CONFIG ] && ./Configure s/compiler:gcc
-    [ -z $NO_CLEAN  ] && make clean
-    make
-    make install_sw
-    cd -
+	cd_repo openssl
+	[ -z $NO_CONFIG ] && ./Configure s/compiler:gcc
+	[ -z $NO_CLEAN ] && make clean
+	[ -z $NO_CLEAN ] && assert_no_error
+	make
+	assert_no_error
+	make install_sw
+	assert_no_error
+	cd_back
 }
 
 function build_libnl()
 {
-    cd ${PATH__SRC__LIBNL}
-    [ -z $NO_CONFIG ] && ./autogen.sh
-    [ -z $NO_CONFIG ] && ./configure --prefix=${PATH__FILESYSTEM} --host=${ARCH} CC=${CROSS_COMPILE}gcc AR=${CROSS_COMPILE}ar
-    [ -z $NO_CLEAN  ] && make clean
-    make
-    make install
-    cd -
+	cd_repo libnl
+	[ -z $NO_CONFIG ] && ./autogen.sh
+	[ -z $NO_CONFIG ] && ./configure --prefix=`path filesystem` --host=${ARCH} CC=${CROSS_COMPILE}gcc AR=${CROSS_COMPILE}ar
+	[ -z $NO_CLEAN ] && make clean
+	[ -z $NO_CLEAN ] && assert_no_error
+	make
+	assert_no_error
+	make install
+	assert_no_error
+	cd_back
 }
 
 function build_wpa_supplicant()
 {
-    cd ${PATH__SRC__HOSTAP}/wpa_supplicant
-    [ -z $NO_CONFIG ] && cp android.config .config
-    [ -z $NO_CONFIG ] && cp ${PATH__SRC__CONFIGURATION}/wpa_supplicant.config ${PATH__FILESYSTEM}/etc/wpa_supplicant.config
-    [ -z $NO_CLEAN  ] && make clean
-    export LIBNL_PATH=${PATH__SRC__LIBNL}
-    export OPENSSL_PATH=${PATH__SRC__OPENSSL}
-    DESTDIR=${PATH__FILESYSTEM} make -j${PROCESSORS_NUMBER}
-    DESTDIR=${PATH__FILESYSTEM} make install
-    cd -
+	cd `repo_path hostap`/wpa_supplicant
+	[ -z $NO_CONFIG ] && cp android.config .config
+	DESTDIR=`path filesystem` make clean
+	assert_no_error
+	DESTDIR=`path filesystem` CFLAGS+="-I`path filesystem`/usr/local/ssl/include -I`path filesystem`/include" LIBS+="-L`path filesystem`/lib -L`path filesystem`/usr/local/ssl/lib -lssl -lcrypto -lm -ldl" LIBS_p+="-L`path filesystem`/lib -L`path filesystem`/usr/local/ssl/lib -lssl -lcrypto -lm -ldl" make -j${PROCESSORS_NUMBER} CC=${CROSS_COMPILE}gcc LD=${CROSS_COMPILE}ld AR=${CROSS_COMPILE}ar
+	assert_no_error
+	DESTDIR=`path filesystem` make install
+	assert_no_error
+	cd_back
 }
 
 function build_hostapd()
 {
-    cd ${PATH__SRC__HOSTAP}/hostapd
-    [ -z $NO_CONFIG ] && cp android.config .config
-    [ -z $NO_CLEAN  ] && make clean
-    export LIBNL_PATH=${PATH__SRC__LIBNL}
-    export OPENSSL_PATH=${PATH__SRC__OPENSSL}
-    DESTDIR=${PATH__FILESYSTEM} make -j${PROCESSORS_NUMBER}
-    DESTDIR=${PATH__FILESYSTEM} make install
-    cd -
+	cd `repo_path hostap`/hostapd
+	[ -z $NO_CONFIG ] && cp android.config .config
+	DESTDIR=`path filesystem` make clean
+	assert_no_error
+	DESTDIR=`path filesystem` CFLAGS+="-I`path filesystem`/usr/local/ssl/include -I`path filesystem`/include" LIBS+="-L`path filesystem`/lib -L`path filesystem`/usr/local/ssl/lib -lssl -lcrypto -lm -ldl" LIBS_p+="-L`path filesystem`/lib -L`path filesystem`/usr/local/ssl/lib -lssl -lcrypto -lm -ldl" make -j${PROCESSORS_NUMBER} CC=${CROSS_COMPILE}gcc LD=${CROSS_COMPILE}ld AR=${CROSS_COMPILE}ar
+	assert_no_error
+	DESTDIR=`path filesystem` make install
+	assert_no_error
+	cd_back
 }
 
 function build_crda()
 {
-    mkdir -p ${PATH__FILESYSTEM}/usr/lib/crda
-    export REG_BIN=${PATH__FILESYSTEM}/usr/lib/crda/regulatory.bin
-    cp ${PATH__SRC__WIRELESS_REGDB}/regulatory.bin ${REG_BIN}
+	export REG_BIN=`path filesystem`/usr/lib/crda/regulatory.bin
+	cp `repo_path wireless_regdb`/regulatory.bin ${REG_BIN}
 
-    cd ${PATH__SRC__CRDA}
-    [ -z $NO_CLEAN] && make clean
-    DESTDIR=${PATH__FILESYSTEM} NLLIBS="-lnl -lnl-genl" NLLIBNAME=libnl-3.0 CFLAGS+="-I${PATH__FILESYSTEM}/usr/local/ssl/include -I${PATH__FILESYSTEM}/include -L${PATH__FILESYSTEM}/usr/local/ssl/lib -L${PATH__FILESYSTEM}/lib" LDLIBS+=-lm USE_OPENSSL=1 UDEV_RULE_DIR="etc/udev/rules.d/" make -j${PROCESSORS_NUMBER} all_noverify CC=${CROSS_COMPILE}gcc LD=${CROSS_COMPILE}ld AR=${CROSS_COMPILE}ar
-    DESTDIR=${PATH__FILESYSTEM} NLLIBS="-lnl -lnl-genl" NLLIBNAME=libnl-3.0 CFLAGS+="-I${PATH__FILESYSTEM}/usr/local/ssl/include -I${PATH__FILESYSTEM}/include -L${PATH__FILESYSTEM}/usr/local/ssl/lib -L${PATH__FILESYSTEM}/lib" LDLIBS+=-lm USE_OPENSSL=1 UDEV_RULE_DIR="etc/udev/rules.d/" make -j${PROCESSORS_NUMBER} install CC=${CROSS_COMPILE}gcc LD=${CROSS_COMPILE}ld AR=${CROSS_COMPILE}ar
-    cd -
+	cd_repo crda
+	[ -z $NO_CLEAN ] && DESTDIR=`path filesystem` make clean
+	[ -z $NO_CLEAN ] && assert_no_error
+	DESTDIR=`path filesystem` NLLIBS="-lnl -lnl-genl" NLLIBNAME=libnl-3.0 CFLAGS+="-I`path filesystem`/usr/local/ssl/include -I`path filesystem`/include -L`path filesystem`/usr/local/ssl/lib -L`path filesystem`/lib" LDLIBS+=-lm USE_OPENSSL=1 UDEV_RULE_DIR="etc/udev/rules.d/" make -j${PROCESSORS_NUMBER} all_noverify CC=${CROSS_COMPILE}gcc LD=${CROSS_COMPILE}ld AR=${CROSS_COMPILE}ar
+	assert_no_error
+	DESTDIR=`path filesystem` NLLIBS="-lnl -lnl-genl" NLLIBNAME=libnl-3.0 CFLAGS+="-I`path filesystem`/usr/local/ssl/include -I`path filesystem`/include -L`path filesystem`/usr/local/ssl/lib -L`path filesystem`/lib" LDLIBS+=-lm USE_OPENSSL=1 UDEV_RULE_DIR="etc/udev/rules.d/" make -j${PROCESSORS_NUMBER} install CC=${CROSS_COMPILE}gcc LD=${CROSS_COMPILE}ld AR=${CROSS_COMPILE}ar
+	assert_no_error
+	cd_back
 }
 
-function build_ti_utils()
+function build_calibrator()
 {
-	cd ${PATH__SRC__TI_UTILS}
-	NFSROOT=${PATH__FILESYSTEM} make
-	NFSROOT=${PATH__FILESYSTEM} make install	
-	cd -	
+	cd_repo ti_utils
+	[ -z $NO_CLEAN ] && NFSROOT=`path filesystem` make clean
+	[ -z $NO_CLEAN ] && assert_no_error
+	NFSROOT=`path filesystem` make
+	assert_no_error
+	NFSROOT=`path filesystem` make install
+	#assert_no_error
+	cd_back
 }
 
 function build_fw_download()
 {
-    cd ${PATH__SRC__FW_DOWNLOAD}
-    mkdir -p $PATH__FILESYSTEM/lib/firmware/ti-connectivity
-    cp ${PATH__SRC__FW_DOWNLOAD}/wl18xx-fw-mc.bin ${PATH__FILESYSTEM}/lib/firmware/ti-connectivity
-    cd -
+	cp `repo_path fw_download`/*.bin `path filesystem`/lib/firmware/ti-connectivity
 }
 
 
 function build_scripts_download()
 {
-    cd ${PATH__SRC__SCRIPTS_DOWNLOAD}   
+	cd_repo scripts_download
 	echo "Copying scripts"
-	for script_dir in `ls ${PATH__SRC__SCRIPTS_DOWNLOAD}`
+	scripts_download_path=`repo_path scripts_download`
+	for script_dir in `ls $scripts_download_path`
 	do
-		echo "Copying everything from ${script_dir} to /home/root directory"
-		mkdir -p ${PATH__FILESYSTEM}/usr/share/wl18xx
-		cp -rf ${PATH__SRC__SCRIPTS_DOWNLOAD}/${script_dir}/* ${PATH__FILESYSTEM}/usr/share/wl18xx
-	done       
-    cd -
+		echo "Copying everything from ${script_dir} to /usr/share/wl18xx directory"
+		cp -rf `repo_path scripts_download`/${script_dir}/* `path filesystem`/usr/share/wl18xx
+	done
+	cd_back
 }
 
 function build_outputs()
 {
-	rm -f ${PATH__OUTPUTS__TAR_FILESYSTEM}
-	rm -f ${PATH__OUTPUTS__UIMAGE}
-	cd ${PATH__FILESYSTEM}
-	tar cpjf ${PATH__OUTPUTS__TAR_FILESYSTEM} .
-	cd -
-	cp ${PATH__TFTP}/uImage ${PATH__OUTPUTS__UIMAGE}
+	rm -f `path outputs`/${tar_filesystem[0]}
+	rm -f `path outputs`/uImage
+	cd_path filesystem
+	tar cpjf `path outputs`/${tar_filesystem[0]} .
+	cd_back
+	cp `path tftp`/uImage `path outputs`/uImage
 }
 
 function install_outputs()
 {
-	cp ${PATH__OUTPUTS__UIMAGE} ${PATH__SERVER__TFTP}/
-	cp ${PATH__OUTPUTS__TAR_FILESYSTEM} ${PATH__SERVER__NFS__SITARA_LEFT}/
-	cp ${PATH__OUTPUTS__TAR_FILESYSTEM} ${PATH__SERVER__NFS__SITARA_RIGHT}/
-	cd ${PATH__SERVER__NFS__SITARA_LEFT}
-	tar xjf ${TAR_FILESYSTEM_SKELETON_FILENAME}
-	cd -
-	cd ${PATH__SERVER__NFS__SITARA_RIGHT}
-	tar xjf ${TAR_FILESYSTEM_SKELETON_FILENAME}
-	cd -
+	tftp_path=${setup[2]}
+	sitara_left_path=${setup[5]}
+	sitara_right_path=${setup[8]}
+
+	cp `path outputs`/uImage ${tftp_path}
+	cp `path outputs`/${tar_filesystem[0]} $sitara_left_path
+	cp `path outputs`/${tar_filesystem[0]} $sitara_right_path
+
+	cd $sitara_left_path
+	tar xjf ${tar_filesystem[0]}
+	cd_back
+
+	cd $sitara_right_path
+	tar xjf ${tar_filesystem[0]}
+	cd_back
+}
+
+files_to_verify=(
+# skeleton path
+# source path
+# pattern in output of file
+
+`path filesystem`/usr/local/sbin/wpa_supplicant
+`repo_path hostap`/wpa_supplicant/wpa_supplicant
+"ELF 32-bit LSB executable, ARM"
+
+`path filesystem`/usr/local/bin/hostapd
+`repo_path hostap`/hostapd/hostapd
+"ELF 32-bit LSB executable, ARM"
+
+`path filesystem`/sbin/crda
+`repo_path crda`/crda
+"ELF 32-bit LSB executable, ARM"
+
+`path filesystem`/usr/lib/crda/regulatory.bin
+`repo_path wireless_regdb`/regulatory.bin
+"CRDA wireless regulatory database file"
+
+`path filesystem`/lib/firmware/ti-connectivity/wl18xx-fw-mc.bin
+`repo_path fw_download`/wl18xx-fw-mc.bin
+"data"
+
+`path filesystem`/lib/modules/3.8.*/extra/drivers/net/wireless/ti/wl18xx/wl18xx.ko
+`repo_path compat_wireless`/drivers/net/wireless/ti/wl18xx/wl18xx.ko
+"ELF 32-bit LSB relocatable, ARM"
+
+`path filesystem`/lib/modules/3.8.13+/extra/drivers/net/wireless/ti/wlcore/wlcore.ko
+`repo_path compat_wireless`/drivers/net/wireless/ti/wlcore/wlcore.ko
+"ELF 32-bit LSB relocatable, ARM"
+
+`path filesystem`/home/root/calibrator
+`repo_path ti_utils`/calibrator
+"ELF 32-bit LSB executable, ARM"
+)
+function verify_skeleton()
+{
+	echo "Verifying filesystem skeleton..."
+
+	i="0"
+	while [ $i -lt ${#files_to_verify[@]} ]; do
+		skeleton_path=${files_to_verify[i]}
+		source_path=${files_to_verify[i + 1]}
+		file_pattern=${files_to_verify[i + 2]}
+		file $skeleton_path | grep "${file_pattern}" >/dev/null
+		assert_no_error
+
+		md5_skeleton=$(md5sum $skeleton_path | awk '{print $1}')
+		md5_source=$(md5sum $source_path     | awk '{print $1}')
+		if [ $md5_skeleton != $md5_source ]; then
+			echo "ERROR: file mismatch"
+			echo $skeleton_path
+			exit 1
+		fi
+		i=$[$i + 3]
+	done
+
+	which regdbdump > /dev/null
+	if [ $? -eq 0 ]; then
+		regdbdump `path filesystem`/usr/lib/crda/regulatory.bin > /dev/null
+		assert_no_error
+	fi
+}
+
+function build_all()
+{
+	setup_directories
+	setup_toolchain
+	setup_repositories
+	setup_branches
+
+	[ -z $NO_CONFIG ] && configure_kernel
+	build_uimage
+	build_modules
+	build_openssl
+	build_libnl
+	build_wpa_supplicant
+	build_hostapd
+	build_crda
+	build_calibrator
+	build_fw_download
+	build_scripts_download
 }
 
 function main()
 {
-   setup_environment
-   setup_directories
-   setup_toolchain
-   setup_repositories
-   setup_branches
+	setup_environment
+	case "$1" in
+		'rebuild')
+		build_all
+		;;
 
-   configure_kernel
-   build_uimage
-   build_modules
-   build_openssl
-   build_libnl
-   build_wpa_supplicant
-   build_hostapd
-   build_crda
-   build_ti_utils
-   build_fw_download
-   build_scripts_download
-   build_outputs
-   [ ! -z INSTALL ] && install_outputs
+		'build')
+		NO_CLEAN=1 NO_CONFIG=1 build_all
+		;;
+
+		'build_kernel')
+		configure_kernel
+		build_uimage
+		;;
+
+		'build_modules')
+		NO_CLEAN=1 NO_CONFIG=1 build_modules
+		;;
+
+		'build_wpa_supplicant')
+		NO_CLEAN=1 NO_CONFIG=1 build_wpa_supplicant
+		;;
+
+		'build_hostapd')
+		NO_CLEAN=1 NO_CONFIG=1 build_hostapd
+		;;
+
+		'build_crda')
+		NO_CLEAN=1 NO_CONFIG=1 build_crda
+		;;
+
+		*)
+		build_all
+		;;
+	esac
+	[ -z $NO_VERIFY ] && verify_skeleton
+	build_outputs
+	[ ! -z $INSTALL_NFS ] && install_outputs
 }
-main
-
+main $1
--- configuration.sh
+++ configuration.sh
@@ -1,71 +1,106 @@
-#---------------------------------------
-# Paths
-export PATH__SERVER__TFTP=/home/barak/Development/wizery/TI/tftp
-export PATH__SERVER__NFS__SITARA_LEFT=/home/barak/Development/wizery/TI/filesystems/SitaraLeft
-export PATH__SERVER__NFS__SITARA_RIGHT=/home/barak/Development/wizery/TI/filesystems/SitaraRight
-export TAR_FILESYSTEM_SKELETON_FILENAME=fs_skeleton.tbz2
+setup=(
+tftp
+192.168.4.220
+/home/barak/Development/wizery/TI/tftp
 
-export PATH__OUTPUTS=${PATH__ROOT}/outputs
-export PATH__OUTPUTS__TAR_FILESYSTEM=${PATH__OUTPUTS}/${TAR_FILESYSTEM_SKELETON_FILENAME}
-export PATH__OUTPUTS__UIMAGE=${PATH__OUTPUTS}/uImage
-export PATH__TOOLCHAIN=${PATH__ROOT}/toolchain
-export PATH__FILESYSTEM=${PATH__ROOT}/fs
-export PATH__TFTP=${PATH__ROOT}/tftp
-export PATH__DOWNLOADS=${PATH__ROOT}/downloads
-export PATH__SRC=${PATH__ROOT}/src
-export PATH__SRC__CONFIGURATION=${PATH__SRC}/configuration
-export PATH__SRC__KERNEL=${PATH__SRC}/kernel
-export PATH__SRC__DRIVER=${PATH__SRC}/driver
-export PATH__SRC__HOSTAP=${PATH__SRC}/hostap
-export PATH__SRC__OPENSSL=${PATH__SRC}/openssl
-export PATH__SRC__LIBNL=${PATH__SRC}/libnl
-export PATH__SRC__COMPAT=${PATH__SRC}/compat
-export PATH__SRC__COMPAT_WIRELESS=${PATH__SRC}/compat-wireless
-export PATH__SRC__CRDA=${PATH__SRC}/crda
-export PATH__SRC__WIRELESS_REGDB=${PATH__SRC}/wireless-regdb
-export PATH__SRC__TI_UTILS=${PATH__SRC}/ti-utils
-export PATH__SRC__FW_DOWNLOAD=${PATH__SRC}/fw-download
-export PATH__SRC__SCRIPTS_DOWNLOAD=${PATH__SRC}/scripts-download
+SitaraLeft
+192.168.4.221
+/home/barak/Development/wizery/TI/filesystems/SitaraLeft
 
-#---------------------------------------
-# Git repositories
-export REPO__URL__CONFIGURATION=git://github.com/barakber/configuration.git
-export REPO__BRANCH__CONFIGURATION=master
+SitaraRight
+192.168.4.222
+/home/barak/Development/wizery/TI/filesystems/SitaraRight
+)
 
-export REPO__URL__KERNEL=git://github.com/ariknem/linux.git
-export REPO__BRANCH__KERNEL=dt-3.8.y
+tar_filesystem=(
+fs_skeleton.tbz2
+)
 
-export REPO__URL__DRIVER=git://github.com/TI-OpenLink/wl18xx.git
-export REPO__BRANCH__DRIVER=mc_internal_310
+toolchain=(
+https://sourcery.mentor.com/GNUToolchain/package6488/public/arm-none-linux-gnueabi/arm-2010q1-202-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
+)
 
-export REPO__URL__HOSTAP=git://github.com/ariknem/hostap.git
-export REPO__BRANCH__HOSTAP=arm_android_jb_mr1_39
+paths=(
+# name
+# path
 
-export REPO__URL__OPENSSL=git://github.com/ariknem/openssl.git
-export REPO__BRANCH__OPENSSL=openssl_arm
+outputs
+${PATH__ROOT}/outputs
 
-export REPO__URL__LIBNL=git://github.com/ariknem/libnl.git
-export REPO__BRANCH__LIBNL=libnl3_arm
+toolchain
+${PATH__ROOT}/toolchain
 
-export REPO__URL__COMPAT=git://github.com/TI-OpenLink/compat.git
-export REPO__BRANCH__COMPAT=dt_310
+filesystem
+${PATH__ROOT}/fs
 
-export REPO__URL__COMPAT_WIRELESS=git://github.com/TI-OpenLink/compat-wireless.git
-export REPO__BRANCH__COMPAT_WIRELESS=dt_310
+tftp
+${PATH__ROOT}/tftp
 
-export REPO__URL__CRDA=git://github.com/mcgrof/crda.git
-export REPO__BRANCH__CRDA=v1.1.3 
+downloads
+${PATH__ROOT}/downloads
 
-export REPO__URL__WIRELESS_REGDB=git://git.kernel.org/pub/scm/linux/kernel/git/linville/wireless-regdb.git
-export REPO__BRANCH__WIRELESS_REGDB=master
+src
+${PATH__ROOT}/src
 
-export REPO__URL__TI_UTILS=git://github.com/TI-OpenLink/ti-utils
-export REPO__BRANCH__TI_UTILS=master
+debugging
+${PATH__ROOT}/debugging
+)
 
-export REPO__URL__FW_DOWNLOAD=git://github.com/TI-OpenLink/wl18xx_fw.git
-export REPO__BRANCH__FW_DOWNLOAD=master
+repositories=(
+# name
+# url
+# branch
 
-export REPO__URL__SCRIPTS_DOWNLOAD=git://github.com/TI-OpenLink/wl12xx_target_scripts.git
-export REPO__BRANCH__SCRIPTS_DOWNLOAD=sitara_310
+configuration
+https://github.com/barakber/configuration.git
+master
 
+kernel
+https://github.com/ariknem/linux.git
+dt-3.8.y
 
+driver
+https://github.com/TI-OpenLink/wl18xx.git
+mc_internal_310
+
+hostap
+https://github.com/TI-OpenLink/hostap.git
+android_jb_mr1_39
+
+openssl
+https://github.com/ariknem/openssl.git
+openssl_arm
+
+libnl
+https://github.com/ariknem/libnl.git
+libnl3_arm
+
+compat
+https://github.com/TI-OpenLink/compat.git
+dt_310
+
+compat_wireless
+https://github.com/TI-OpenLink/compat-wireless.git
+dt_310
+
+crda
+https://github.com/mcgrof/crda.git
+v1.1.3
+
+wireless_regdb
+https://git.kernel.org/pub/scm/linux/kernel/git/linville/wireless-regdb.git
+master
+
+ti_utils
+https://github.com/TI-OpenLink/18xx-ti-utils.git
+master
+
+fw_download
+https://github.com/TI-OpenLink/wl18xx_fw.git
+master
+
+scripts_download
+https://github.com/TI-OpenLink/wl12xx_target_scripts.git
+sitara_310
+
+)
